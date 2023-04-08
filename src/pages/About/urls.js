549a99c03efb4d167b49051dc0c846724a869936
joanartdacosta <joanartdacosta@gmail.com>
2023-04-08 02:02:26 +0200

    add urls about gallery

--- src/pages/About/urls.js
+++ src/pages/About/urls.js
@@ -1,16 +1,20 @@
 const urls = [
-  { id: "1", text: "", url: "url(../images/gallery/theatre.png)" },
-  "url(../images/gallery/dance.png)",
-  "url(../images/gallery/cook.jpg)",
-  "url(../images/gallery/painting.jpg)",
-  "url(../images/gallery/travel.jpg)",
-  "url(../images/gallery/hiking.jpg)",
-  "url(../images/gallery/workout.png)",
-  "url(../images/gallery/podcasts.png)",
-  "url(../images/gallery/write.jpg)",
-  "url(../images/gallery/gameboard.jpg)",
-  "url(../images/gallery/standup.jpg)",
-  "url(../images/gallery/chocolate.jpg)",
+  { id: 1, text: "Acting", url: "url(../images/gallery/theatre.png)" },
+  { id: 2, text: "Dance", url: "url(../images/gallery/dance.png)" },
+  { id: 3, text: "Cooking", url: "url(../images/gallery/cook.jpg)" },
+  { id: 4, text: "Painting", url: "url(../images/gallery/painting.jpg)" },
+  { id: 5, text: "Travelling", url: "url(../images/gallery/travel.jpg)" },
+  { id: 6, text: "Hiking", url: "url(../images/gallery/hiking.jpg)" },
+  { id: 6, text: "Workout", url: "url(../images/gallery/workout.png)" },
+  { id: 7, text: "Podcasts", url: "url(../images/gallery/podcasts.png)" },
+  { id: 8, text: "Writing", url: "url(../images/gallery/write.jpg)" },
+  { id: 9, text: "Gameboard", url: "url(../images/gallery/gameboard.jpg)" },
+  { id: 10, text: " ", url: "url(../images/gallery/standup.jpg)" },
+  {
+    id: 11,
+    text: "Chocolate Lover",
+    url: "url(../images/gallery/chocolate.jpg)",
+  },
 ];
 
 export default urls;
