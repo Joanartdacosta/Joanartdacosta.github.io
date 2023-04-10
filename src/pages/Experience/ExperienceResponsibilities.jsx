db282ee0616dffeb3ac6dce5be18805b822bffd5
joanartdacosta <joanartdacosta@gmail.com>
2023-04-10 14:32:40 +0200

    cleaning-code-using-map-and-arrays

--- src/pages/Experience/ExperienceResponsibilities.jsx
+++ src/pages/Experience/ExperienceResponsibilities.jsx
@@ -1,65 +1,41 @@
 import React from "react";
+import codingskills from "./codingskills";
+import responsibilities from "./responsibilties";
+import Card from "../../components/card/Card";
+
+function createCard(codingskill) {
+  return (
+    <Card
+      key={codingskill.id}
+      name={codingskill.name}
+      description={codingskill.description}
+    />
+  );
+}
+
+function createCardR(responsibility) {
+  return (
+    <Card key={responsibility.id} description={responsibility.description} />
+  );
+}
 
 function ExperienceResponsibilities() {
   return (
-    <div className="experience-responsibilities">
-      <div>
-        <p>My responsibilities included:</p>
-        <ul>
-          <li>
-            Project management of the clinical trials - involvement in internal
-            and external meetings.
-          </li>
-          <li>
-            Training of health professionals (doctors, nurses, laboratory
-            technicians, others) when a new substance arises.
-          </li>
-          <li>
-            Answering medical questions and giving advice if they needed for a
-            patient with specific condition.
-          </li>
-          <li>
-            {" "}
-            Monitoring and audit the hospitals to guarantee the safety of the
-            patients and also, the accuracy of the data.
-          </li>
-          <li>
-            Quality assurance activities, including an External Audit in a
-            company in Canada
-          </li>
-          <li>
-            {" "}
-            Meetings & presentations with doctors to clarify some questions
-            about new substances, sharing scientific information (as a trainer).
-          </li>
-          <li>
-            {" "}
-            Data-Management - management of the data-basis used in the clinical
-            trials.
-          </li>
-        </ul>
+    <div>
+      <div className="experience-responsibilities">
+        <div>
+          <p>My responsibilities included:</p>
 
-        <h3>Now, you think - how can this could relate with coding?</h3>
-        <ul>
-          <li>
-            {" "}
-            Monitoring Activities & SOPs & Audit Experiences: <br />
-            Experience with documentation's (paying attention to detail, reading
-            the Guidelines)
-          </li>
-          <li>
-            {" "}
-            Clinical Trial Management: <br /> Patience and ability to face the
-            problems, focus on trying to find a solution
-          </li>
-          <li>Working alone, autonomy</li>
-          <li>
-            Scientific Advices & Trainings: <br />
-            Communication skills, ability to simplify a complex and technical
-            concept when it is necessary to explain to a client & audience
-            (important in client meetings & others)
-          </li>
-        </ul>
+          {responsibilities.map(createCardR)}
+          <p>{responsibilities.name}</p>
+          <p>{responsibilities.description}</p>
+
+          <h3>Now, you think - how can this could relate with coding?</h3>
+
+          {codingskills.map(createCard)}
+          <p>{codingskills.name}</p>
+          <p>{codingskills.description}</p>
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/Experience/responsibilties.jsx
@@ -0,0 +1,39 @@
+const responsibilities = [
+  {
+    id: 1,
+    description:
+      "Project management of the clinical trials - involvement in internal and external meetings.",
+  },
+  {
+    id: 2,
+    description:
+      "Training of health professionals (doctors, nurses, laboratory technicians, others) when a new substance arises.",
+  },
+  {
+    id: 3,
+    description:
+      "Answering medical questions and giving advice if they needed for a patient with specific condition.",
+  },
+  {
+    id: 4,
+    description:
+      "Monitoring and audit the hospitals to guarantee the safety of the patients and also, the accuracy of the data.",
+  },
+  {
+    id: 5,
+    description:
+      "Quality assurance activities, including an External Audit in a company in Canada.",
+  },
+  {
+    id: 6,
+    description:
+      "Meetings & presentations with doctors to clarify some questions about new substances, sharing scientific information (as a trainer).",
+  },
+  {
+    id: 7,
+    description:
+      "Data-Management - management of the data-basis used in the clinical trials.",
+  },
+];
+
+export default responsibilities;
