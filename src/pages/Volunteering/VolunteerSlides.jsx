e031b04995cfc833288362ad458d6676dd8c41bd
joanartdacosta <joanartdacosta@gmail.com>
2023-04-10 18:45:35 +0200

    using map method in volunteer gallery

--- src/pages/Volunteering/VolunteerSlides.jsx
+++ src/pages/Volunteering/VolunteerSlides.jsx
@@ -3,54 +3,15 @@ import volunteers from "./volunteers";
 import CardVolunteer from "./CardVolunteer";
 
 function createCard(volunteer) {
-  return <CardVolunteer key={volunteer.id} img={volunteer.imgURL} />;
+  return <CardVolunteer name={volunteer.name} imgURL={volunteer.imgURL} />;
 }
 
 function VolunteerSlides() {
-  return <div className="volunteer-gallery">{volunteers.map(createCard)}</div>;
+  return (
+    <div className="volunteer-gallery">
+      {volunteers.map((volunteer) => createCard(volunteer))}
+    </div>
+  );
 }
-
-//           src="../images/volunteer/1.png"
-//           alt=""
-//         ></img>
-//       </div>
-//       <div className="column">
-//         <img
-//           className="demo cursor volunteer-image"
-//           src="../images/volunteer/2.jpg"
-//           alt=""
-//         ></img>
-//       </div>
-//       <div className="column">
-//         <img
-//           className="demo cursor volunteer-image"
-//           src="../images/volunteer/3.jpg"
-//           alt=""
-//         ></img>
-//       </div>
-//       <div className="column">
-//         <img
-//           className="demo cursor volunteer-image"
-//           src="../images/volunteer/4.jpg"
-//           alt=""
-//         ></img>
-//       </div>
-//       <div className="column">
-//         <img
-//           className="demo cursor volunteer-image"
-//           src="../images/volunteer/5.jpg"
-//           alt=""
-//         ></img>
-//       </div>
-//       <div className="column">
-//         <img
-//           className="demo cursor volunteer-image"
-//           src="../images/volunteer/6.png"
-//           alt=""
-//         ></img>
-//       </div>
-//     </>
-//   );
-// }
 
 export default VolunteerSlides;
--- src/pages/Volunteering/volunteers.jsx
+++ src/pages/Volunteering/volunteers.jsx
@@ -2,32 +2,32 @@ const volunteers = [
   {
     id: 1,
     name: "1",
-    imgURL: "url(../../images/volunteer/1.png)",
+    imgURL: "../images/volunteer/1.png",
   },
   {
     id: 2,
     name: "2",
-    imgURL: "url(../images/volunteer/2.jpg)",
+    imgURL: "../images/volunteer/2.jpg",
   },
   {
     id: 3,
     name: "3",
-    imgURL: "url(../images/volunteer/3.jpg)",
+    imgURL: "../images/volunteer/3.jpg",
   },
   {
     id: 4,
     name: "4",
-    imgURL: "url(../images/volunteer/4.jpg)",
+    imgURL: "../images/volunteer/4.jpg",
   },
   {
     id: 5,
     name: "5",
-    imgURL: "url(../images/volunteer/5.jpg)",
+    imgURL: "../images/volunteer/5.jpg",
   },
   {
     id: 6,
     name: "6",
-    imgURL: "url(../images/volunteer/6.png)",
+    imgURL: "../images/volunteer/6.png",
   },
 ];
 
