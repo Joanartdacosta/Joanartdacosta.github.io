ab1f4e741ebffc0c70f1a2cc54220e449a1289e4
joanartdacosta <joanartdacosta@gmail.com>
2023-04-17 15:37:40 +0200

    add photo

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Social from "../../components/social/Social";
+import { Link } from "react-router-dom";
 
 function Contacts() {
   return (
@@ -14,14 +15,22 @@ function Contacts() {
           transition={{ ease: "easeOut", duration: 2 }}
         >
           <p>
-            I will be very happy to welcome you. For more information about me,
-            services or collaborations you can contact me: <br />
+            I will be very happy to welcome you. <br />
+            For more information or collaborations you can contact me: <br />
             Email: jrtdacosta@gmail.com. <br />
             Phone: +351 91 744 97 34
           </p>
         </motion.div>
 
         <Social />
+
+        <Link to="/about">
+          <img
+            className="home-photo"
+            src="../../images/general/JC.png"
+            alt=""
+          ></img>
+        </Link>
       </div>
     </div>
   );
