0782c64d8735d6ae3ad6bf174e3a410d558f2c75
joanartdacosta <joanartdacosta@gmail.com>
2023-06-27 17:43:31 +0200

    projects images

--- src/pages/Projects/ProjectsGallery.jsx
+++ src/pages/Projects/ProjectsGallery.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import projects from "./Projects";
 import CardProject from "./CardProject";
+import chalets from "./Chalet";
+import roads from "./Roads";
+import reunions from "./Reunions";
 
 function createCard(project) {
   return <CardProject name={project.name} imgURL={project.imgURL} />;
@@ -9,7 +11,16 @@ function createCard(project) {
 function ProjectsGallery() {
   return (
     <div className="projects-gallery">
-      {projects.map((project) => createCard(project))}
+      <p>
+        <a href="https://www.instagram.com/chalet_designinteriores/">
+          Chalet - Interior Design -
+        </a>
+      </p>
+      {chalets.map((chalet) => createCard(chalet))}
+      <p>Estrada Fora - Travel Blog</p>
+      {roads.map((road) => createCard(road))}
+      <p>Reunion - Bootcamp's final project</p>
+      {reunions.map((reunion) => createCard(reunion))}
     </div>
   );
 }
---
+++ src/pages/Projects/Roads.jsx
@@ -0,0 +1,22 @@
+const roads = [
+  {
+    id: 1,
+    name: "Estradas - Travel Blog",
+    imgURL:
+      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/Estradas01.png",
+  },
+  {
+    id: 2,
+    name: "Estradas - Travel Blog",
+    imgURL:
+      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/Estradas02.png",
+  },
+  {
+    id: 3,
+    name: "Estradas - Travel Blog",
+    imgURL:
+      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/Estradas04.png",
+  },
+];
+
+export default roads;
