1045eeb66797c2139c030296092ea1dcc24cf6f3
joanartdacosta <joanartdacosta@gmail.com>
2023-07-07 23:11:51 +0200

    change photos of chalet

--- src/pages/Projects/Chalet.jsx
+++ src/pages/Projects/Chalet.jsx
@@ -3,19 +3,26 @@ const chalets = [
     id: 1,
     name: "Chalet - Interior Design",
     imgURL:
-      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/Chalet1.png",
+      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/figma-chalet/Chalet-homepage1.png",
   },
   {
     id: 2,
     name: "Chalet - Interior Design",
     imgURL:
-      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/Chalet2.png",
+      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/figma-chalet/Chalet-homepage2.png",
   },
   {
     id: 3,
     name: "Chalet - Interior Design",
     imgURL:
-      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/ChaletPalette.png",
+      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/figma-chalet/Chalet-aboutme.png",
+  },
+
+  {
+    id: 4,
+    name: "Chalet - Interior Design",
+    imgURL:
+      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/figma-chalet/Chalet-palette.png",
   },
 ];
 
