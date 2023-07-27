2d22158a769dcb9ff39958e5a9ce72f94b119fe0
joanartdacosta <joanartdacosta@gmail.com>
2023-07-27 15:14:02 +0200

    update gallery

--- src/pages/Projects/Reunions.jsx
+++ src/pages/Projects/Reunions.jsx
@@ -3,19 +3,19 @@ const reunions = [
     id: 1,
     name: "Reunion - Bootcamp's final project.",
     imgURL:
-      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/Reunion1.png",
+      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/websites/reunion/Reunion1.png",
   },
   {
     id: 2,
     name: "Reunion - Bootcamp's final project.",
     imgURL:
-      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/Reunion4.png",
+      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/websites/reunion/Reunion4.png",
   },
   {
     id: 3,
     name: "Reunion - Bootcamp's final project.",
     imgURL:
-      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/Reunion5.png",
+      "https://joana-personal-website.s3.eu-central-1.amazonaws.com/websites/reunion/Reunion5.png",
   },
 ];
 
