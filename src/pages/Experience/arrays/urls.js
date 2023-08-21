b1d9ed361bc5cca12b4ab18f56abd1d641cf5822
joanartdacosta <joanartdacosta@gmail.com>
2023-08-21 12:14:45 +0200

    change structure of experience tasks

--- src/pages/Experience/arrays/urls.js
+++ src/pages/Experience/arrays/urls.js
@@ -7,41 +7,29 @@ const urls = [
   {
     id: 2,
     text:
-      "Training of health professionals (doctors, nurses, laboratory technicians, others)" +
-      "when a new substance arises.",
+      "Training of health professionals, sharing scientific information." +
+      " Meetings with doctors. Answering medical questions and giving scientific advice.",
     url: "url(https://images.unsplash.com/photo-1519452575417-564c1401ecc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)",
   },
   {
     id: 3,
-    text: "Answering medical questions and giving advice if they needed for a patient with specific condition.",
-    url: "url(https://images.unsplash.com/photo-1453847668862-487637052f8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80)",
-  },
-  {
-    id: 4,
     text:
       "Monitoring and audit the hospitals to guarantee the safety of the patients and also," +
       "the accuracy of the data.",
     url: "url(https://plus.unsplash.com/premium_photo-1661751531192-a2c9ce625ad3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)",
   },
   {
-    id: 5,
+    id: 4,
     text: "Quality assurance activities, including an External Audit in a company in Canada.",
     url: "url(https://images.unsplash.com/photo-1569235186275-626cb53b83ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80)",
   },
   {
-    id: 6,
-    text:
-      "Meetings & presentations with doctors to clarify some questions about new substances," +
-      "sharing scientific information (as a trainer).",
-    url: "url(https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80)",
-  },
-  {
-    id: 7,
+    id: 5,
     text: "Data-Management: management of the data-basis used in the clinical trials.",
     url: "url(https://images.unsplash.com/photo-1516383274235-5f42d6c6426d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80)",
   },
   {
-    id: 8,
+    id: 6,
     text: "Writing Standard Operation Procedures (SOP), Risk Assessments and Scientific articles.",
     url: "url(https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80)",
   },
