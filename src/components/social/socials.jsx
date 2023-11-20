3f788a4903b9c9ce7e495bc519f62b46928322ea
joanartdacosta <joanartdacosta@gmail.com>
2023-11-20 21:42:49 +0100

    update cv

--- src/components/social/socials.jsx
+++ src/components/social/socials.jsx
@@ -15,7 +15,7 @@ const SOCIALS = [
     id: 3,
     href: "https://joana-personal-website.s3.eu-central-1.amazonaws.com/CV+JC.pdf",
     text: "CV",
-    url: "https://joana-personal-website.s3.eu-central-1.amazonaws.com/cv.png",
+    url: "https://joana-personal-website.s3.eu-central-1.amazonaws.com/Joana+Costa_11.2023.pdf",
   },
 ];
 
