17c06bed3256bd55e01ffa9584c422370eeec79f
joanartdacosta <joanartdacosta@gmail.com>
2023-11-22 18:45:03 +0100

    fix navbar and cards

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ import NavBarHamburguer from "./navbar-hamburguer/NavBarHamburger";
 export default function NavBar() {
   return (
     <div>
-      <div className="w-full bg-black navbar menu-normal justify-center">
+      <div className="w-full bg-navbar navbar menu-normal justify-center">
         <div>
           <LinksInline />
         </div>
