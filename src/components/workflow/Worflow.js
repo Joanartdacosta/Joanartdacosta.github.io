63ec74034c9c4533e01692f0861451f5ee25add5
joanartdacosta <joanartdacosta@gmail.com>
2023-11-30 13:31:52 +0100

    add workflow text with tailwind component

---
+++ src/components/workflow/Worflow.js
@@ -0,0 +1,118 @@
+// https://flowbite.com/docs/components/timeline/
+
+export default function Workflow() {
+  return (
+    <div>
+      <ol class="relative border-s border-gray-200 dark:border-gray-700">
+        <li class="mb-10 ms-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            Discussion
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Application UI code in Tailwind CSS
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            We discuss the project with you, define the goals of the future
+            website and your preferences in design. We draw up a detailed
+            technical task for website development.
+          </p>
+          <div class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+            Learn more{" "}
+            <svg
+              class="w-3 h-3 ms-2 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </div>
+        </li>
+        <li class="mb-10 ms-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            March 2022
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Comercial Proposal
+          </h3>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+            I send you a commercial proposal with the scpeficied scope of work,
+            terms and payment rates to choose form. We sign a contract with the
+            cooperation option you have choosen.
+          </p>
+        </li>
+        <li class="ms-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            Website Structure
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            E-Commerce UI code in Tailwind CSS
+          </h3>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+            I collect all the information about your business, analyze
+            competitor's sites, their pros and cons, research your trget
+            audicence, determine its needs and characteristics. I develop the
+            structure of the site, the order of blocks and their content. I
+            think trought the logic of interaction with the user. I'm working on
+            the texts.
+          </p>
+        </li>
+        <li class="ms-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            Design
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Full Design
+          </h3>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+            On the basis of references on visual style I create a design concept
+            of the site on the example of the first threen sections of the
+            hompeage. If necessary, we make corrections and approve it. After
+            approval of the design concept, I create a desngn layout for the
+            entire site. Design layouts are presented and agreed on a video call
+            wth the reationale for the decisions made.
+          </p>
+        </li>{" "}
+        <li class="ms-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            Website Structure
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            Site Developing
+          </h3>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+            At this stage, your site comes to life and turns from a layout into
+            a full-fledged working business tool.
+          </p>
+        </li>{" "}
+        <li class="ms-4">
+          <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            Website Structure
+          </time>
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+            E-Commerce UI code in Tailwind CSS
+          </h3>
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+            After the site is ready and the payment is made, I record a video
+            tutorial on how to manage the wesbite. I will be in touch with you
+            to answer questions about the site. You ca anlso contact me for
+            further support of the project.
+          </p>
+        </li>
+      </ol>
+    </div>
+  );
+}
